5a56613d21eea226a2f2f3e4464ba46294f484b7
Ella475 <Ellashalom1995@gmail.com>
2022-09-03 01:14:01 +0300

    Connected to meta-mask :)

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import { BackgroundScreen, Gif } from './GlobalStyle';
 
 class App extends Component {
+    state = { loading: true, drizzleState: null };
+
     render() {
       return <BackgroundScreen main><Gif></Gif></BackgroundScreen>
     }
+
+    componentDidMount() {
+        const { drizzle } = this.props;
+    
+        this.unsubscribe = drizzle.store.subscribe(() => {
+          const drizzleState = drizzle.store.getState();
+          if (drizzleState.drizzleStatus.initialized) {
+            this.setState({ loading: false, drizzleState }, () => {});
+          }
+        });
+      }
+    
+      componentWillUnmount() {
+        this.unsubscribe();
+      }
 }
 
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom';
 import { Drizzle, generateStore } from '@drizzle/store';
 import App from './App';
+import Vote from 'contracts/Vote.json';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const options = {
+    contracts: [Vote]
+};
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+  
+ReactDOM.render(<App drizzle={drizzle}/>, document.getElementById('root'));
